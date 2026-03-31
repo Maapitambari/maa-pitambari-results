ea01253cd3314d3525c52a8b91bdc9722a07f0c0
Maapitambari <shyamsundarnetzone@gmail.com>
2026-03-31 21:52:07 +0530

    Delete firebase.js

--- firebase.js
+++
@@ -1,9 +0,0 @@
-onst firebaseConfig = {
-  apiKey: "PASTE_HERE",
-  authDomain: "PASTE_HERE",
-  projectId: "PASTE_HERE",
-};
-
-firebase.initializeApp(firebaseConfig);
-const auth = firebase.auth();
-const db = firebase.firestore();
